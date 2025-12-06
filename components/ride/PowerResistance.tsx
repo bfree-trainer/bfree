3d091db200c8a0be8536c9ca02f1f8d38080054f
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-12-07 00:31:27 +0100

    Better min/max P selection

--- components/ride/PowerResistance.tsx
+++ components/ride/PowerResistance.tsx
@@ -1,7 +1,13 @@
-import TextField from '@mui/material/TextField';
+import Slider from '@mui/material/Slider';
 import RideSetupCard, { SetupFormControl } from 'components/ride/RideSetupCard';
 
 export type PowerLimits = { min: number; max: number };
+
+const minDistance = 100;
+
+function valuetext(value: number) {
+	return `${value} W`;
+}
 
 export default function PowerResistance({
 	limits,
@@ -10,33 +16,33 @@ export default function PowerResistance({
 	limits: { min: number; max: number };
 	setLimits: (limits: PowerLimits) => void;
 }) {
+	const handleChange = (event: Event, newValue: number[], activeThumb: number) => {
+		if (newValue[1] - newValue[0] < minDistance) {
+			if (activeThumb === 0) {
+				const clamped = Math.min(newValue[0], 1000 - minDistance);
+				setLimits({ min: clamped, max: clamped + minDistance });
+			} else {
+				const clamped = Math.max(newValue[1], minDistance);
+				setLimits({ min: clamped - minDistance, max: clamped });
+			}
+		} else {
+			setLimits({ min: newValue[0], max: newValue[1] });
+		}
+	};
+
 	return (
 		<RideSetupCard title="Power" image="/images/cards/slope.jpg">
 			<SetupFormControl>
-				<TextField
-					value={limits.min || 0}
-					error={limits.min < 0}
-					onChange={
-						// @ts-ignore
-						(e) => setLimits({ ...limits, min: Number(e.target.value) || 0 })
-					}
-					id="outlined-basic"
-					label="Min"
-					variant="outlined"
-				/>
-			</SetupFormControl>
-			<br />
-			<SetupFormControl>
-				<TextField
-					value={limits.max || 0}
-					error={limits.max <= 0}
-					onChange={
-						// @ts-ignore
-						(e) => setLimits({ ...limits, max: Number(e.target.value) || 0 })
-					}
-					id="outlined-basic"
-					label="Max"
-					variant="outlined"
+				<Slider
+					getAriaLabel={() => 'Min and max power'}
+					value={[ limits.min, limits.max ]}
+					min={0}
+					max={1000}
+					onChange={handleChange}
+					valueLabelDisplay="auto"
+					valueLabelFormat={valuetext}
+					getAriaValueText={valuetext}
+					disableSwap
 				/>
 			</SetupFormControl>
 		</RideSetupCard>
