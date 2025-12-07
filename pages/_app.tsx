e1bcb391b50e0825f0b7b1e414aa8593a4ba5e6a
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-12-07 22:55:18 +0100

    prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import 'styles/globals.css'
+import 'styles/globals.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import { red } from '@mui/material/colors';
 import PropTypes from 'prop-types';
@@ -12,8 +12,8 @@ import { CacheProvider } from '@emotion/react';
 import { useEffect } from 'react';
 
 export const cache = createCache({
-  key: 'css',
-  prepend: true,
+	key: 'css',
+	prepend: true,
 });
 
 // Create a theme instance.
@@ -35,26 +35,26 @@ const theme = createTheme({
 });
 
 function App({ Component, pageProps }) {
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
+	useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector('#jss-server-side');
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles);
+		}
+	}, []);
 
-  return (
-    <CacheProvider value={cache}>
-		<ThemeProvider theme={theme}>
-			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-			<CssBaseline />
-			<Component {...pageProps} />
-		</ThemeProvider>
-    </CacheProvider>
-  );
+	return (
+		<CacheProvider value={cache}>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</CacheProvider>
+	);
 }
 
-export default App
+export default App;
 
 App.propTypes = {
 	Component: PropTypes.elementType.isRequired,
