d6155e714fb1242809432e1a7e1ee9c8083c51f0
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-12-07 14:27:02 +0100

    env lic

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: Olli Vanhoja <olli.vanhoja@gmail.com>
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
