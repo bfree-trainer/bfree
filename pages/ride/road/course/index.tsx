a8146c220975d7ed505eb8ab6f9765be4d594ab0
Olli Vanhoja <olli.vanhoja@gmail.com>
2026-04-11 17:34:03 +0000

    gpx export

--- pages/ride/road/course/index.tsx
+++ pages/ride/road/course/index.tsx
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import { useCallback, useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import MyHead from 'components/MyHead';
@@ -11,8 +12,26 @@ import RoutePlannerPanel from 'components/RoutePlannerPanel';
 import BottomNavi from 'components/BottomNavi';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import IconDownload from '@mui/icons-material/GetApp';
+import { CourseData, courseData2gpx } from 'lib/gpx_parser';
+import downloadBlob from 'lib/download_blob';
 
 export default function RoadCourse() {
+	const [course, setCourse] = useState<CourseData | null>(null);
+	const [courseName, setCourseName] = useState('Untitled');
+
+	const handleCourseChange = useCallback((c: CourseData | null, name: string) => {
+		setCourse(c);
+		setCourseName(name);
+	}, []);
+
+	const handleDownload = () => {
+		if (!course) return;
+		const gpx = courseData2gpx(course);
+		const blob = new Blob([gpx], { type: 'application/gpx+xml' });
+		const filename = `${courseName || 'course'}.gpx`;
+		downloadBlob(blob, filename);
+	};
+
 	return (
 		<Container maxWidth="md">
 			<MyHead title="Course" />
@@ -21,15 +40,14 @@ export default function RoadCourse() {
 				<Typography variant="body1" color="text.primary" sx={{ mt: 2, mb: 2 }}>
 					Plan a route or import a GPX file.
 				</Typography>
-				<RoutePlannerPanel />
+				<RoutePlannerPanel onCourseChange={handleCourseChange} />
 			</Box>
 			<BottomNavi>
 				<BottomNavigationAction
 					label="Download"
 					icon={<IconDownload />}
-					onClick={(e) => {
-						e.preventDefault();
-					}}
+					disabled={!course}
+					onClick={handleDownload}
 				/>
 			</BottomNavi>
 		</Container>
